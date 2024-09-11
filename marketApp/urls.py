ec87fda3eaae6c2762bf93edbdefaf7841ea6753
denisbelenko <denisdel123@>
2024-09-11 15:07:06 +0700

    перенастроил права доступа, добавил slug поле в модель, добавил метод генерации slug-имя, реализовал эндпоинты для изменения и удаления категрий, настроил пагинацию страниц в категории

--- marketApp/urls.py
+++ marketApp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
 from marketApp.apps import MarketappConfig
-from marketApp.views import CategoryCreateAPIView, CategoryListAPIView
+from marketApp.views import CategoryCreateAPIView, CategoryListAPIView, CategoryUpdateAPIView, CategoryDestroyAPIView
 
 app_name = MarketappConfig.name
 
 urlpatterns = [
     path('category/create/', CategoryCreateAPIView.as_view(), name='category-create'),
     path('category/list/', CategoryListAPIView.as_view(), name='category-list'),
+    path('category/update/<int:pk>/', CategoryUpdateAPIView.as_view(), name='category-update'),
+    path('category/destroy/<int:pk>/', CategoryDestroyAPIView.as_view(), name='category-destroy'),
 ]
