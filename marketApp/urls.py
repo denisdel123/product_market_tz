05e38789c475d95b87dc15539fd9d8ca89b70700
denisbelenko <denisdel123@>
2024-09-11 13:35:19 +0700

    Описал сериализатор для категории, реализовал эндпоинты создания и вывода категорий

--- marketApp/urls.py
+++ marketApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
 from marketApp.apps import MarketappConfig
+from marketApp.views import CategoryCreateAPIView, CategoryListAPIView
 
 app_name = MarketappConfig.name
 
 urlpatterns = [
-
+    path('category/create/', CategoryCreateAPIView.as_view(), name='category-create'),
+    path('category/list/', CategoryListAPIView.as_view(), name='category-list'),
 ]
--- marketApp/views.py
+++ marketApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from marketApp.models import Category
+from marketApp.serializers import CategorySerializer
+
+
+class CategoryCreateAPIView(generics.CreateAPIView):
+    serializer_class = CategorySerializer
+
+
+class CategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
