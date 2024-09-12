bf8e54d5f44c92099eb2071974d70716ac1f4e08
denisbelenko <denisdel123@>
2024-09-12 12:56:30 +0700

    Реализовал модель подкатегорий, создал миграцию

--- marketApp/models.py
+++ marketApp/models.py
@@ -23,7 +23,6 @@ class Category(models.Model):
     )
 
     def save(self, *args, **kwargs):
-
         if not self.slug:
             transliterated_name = translit(self.name, language_code='ru', reversed=False)
             self.slug = slugify(transliterated_name)
@@ -36,4 +35,45 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+        ordering = ['name']
+
+
+class Subcategory(models.Model):
+    photo = models.ImageField(
+        upload_to='subcategory/',
+        **NULLABLE, verbose_name='Картинка',
+        help_text='Загрузите картинку',
+    )
+
+    name = models.CharField(
+        max_length=30,
+        verbose_name='Наименование',
+        help_text='Введите наименование',
+        unique=True
+    )
+    slug = models.SlugField(
+        max_length=30,
+        unique=True,
+        **NULLABLE
+    )
+    associated_category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        verbose_name='Категории',
+        related_name='related_category',
+        help_text='Выберете категорию'
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            transliterated_name = translit(self.name, language_code='ru', reversed=False)
+            self.slug = transliterated_name
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Подкатегория'
+        verbose_name_plural = 'Подкатегории'
         ordering = ['name']
