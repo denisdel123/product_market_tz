42871f67e259f3d2159c861868a43bb80d455a23
denisbelenko <denisdel123@>
2024-09-17 11:35:43 +0700

    изменил код создания администратора

--- usersApp/management/commands/csu.py
+++ usersApp/management/commands/csu.py
@@ -10,10 +10,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
             first_name='Admin',
-            email='admin@mail.ru',
+            email=os.environ.get('EMAIL_SUPERUSER'),
             is_active=True,
             is_staff=True,
             is_superuser=True
         )
-        user.set_password(os.environ.get('PASS_SUPERUSER'))
+        user.set_password(os.environ.get('PASSWORD_SUPERUSER'))
         user.save()
