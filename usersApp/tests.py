63f499fece08eb62868605b2b9eed9c4e11a1e32
denisbelenko <denisdel123@>
2024-09-17 13:12:21 +0700

    установил пакет для тестирования и написал тесты для модели User

--- usersApp/tests.py
+++ usersApp/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from usersApp.models import User
+
+
+class UserTestCase(APITestCase):
+
+    def test_user_registration(self):
+        data = {
+            "email": "test@mail.ru",
+            "password1": "Q23wer344",
+            "password2": "Q23wer344"
+        }
+
+        url = reverse('usersApp:user-create')
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_user_registration_invalid_data(self):
+        data = {
+            "email": "test-32",
+            "password1": "55555",
+            "password2": "55555"
+        }
+
+        url = reverse('usersApp:user-create')
+
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
