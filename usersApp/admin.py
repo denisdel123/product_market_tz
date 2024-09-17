9d3788fd81445cb38ebf0f4ca67376df28eacb44
denisbelenko <denisdel123@>
2024-09-17 11:55:50 +0700

    подключил админку

--- usersApp/admin.py
+++ usersApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from usersApp.models import User
 
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'email', 'city', 'phone', 'age']
