dafce9a336a5099a5e1408e1528632b730af1024
denisbelenko <denisdel123@>
2024-09-17 11:55:29 +0700

    подключил админку

--- marketApp/admin.py
+++ marketApp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from marketApp.models import Category, Subcategory, Product, CartView
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['photo', 'name', 'slug']
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ['photo', 'name', 'slug', 'associated_category']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['photo', 'name', 'price', 'slug', 'associated_subcategory']
+
+
+@admin.register(CartView)
+class CartViewAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'product', 'quantity']
