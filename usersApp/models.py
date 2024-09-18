7b64c7880a4bffe80d683eeb59bac2578aea06ad
denisbelenko <denisdel123@>
2024-09-18 11:54:54 +0700

    дописал модель добавив метод __str__ и класс Meta

--- usersApp/models.py
+++ usersApp/models.py
@@ -15,3 +15,11 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.first_name}, {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ['email']
